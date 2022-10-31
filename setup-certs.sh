6b741c68455cbd021a630a83e4424665ba7a7627
Luke Bakken <luke@bakken.io>
2022-10-31 16:34:16 -0700

    Use same CA for all certs, add setup script

---
+++ setup-certs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# https://stackoverflow.com/a/1710543
+# shellcheck disable=SC2155
+
+set -e
+set -u
+
+readonly curdir="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)"
+readonly certs_root_dir="$curdir/tls-gen/basic"
+readonly certs_result_dir="$certs_root_dir/result"
+readonly ca_cert_pem="$certs_result_dir/ca_certificate.pem"
+
+readonly rmq_certs="$curdir/certs"
+
+# This generates the CA, as well as server/client certs for rmq0
+make -C "$certs_root_dir" 'CN=rmq0'
+
+# This usese the CA above and generates the server/client certs for rmq1
+make -C "$certs_root_dir" 'CN=rmq1' gen-server
+make -C "$certs_root_dir" 'CN=rmq1' gen-client
+
+cp -vf "$ca_cert_pem" "$rmq_certs"
+
+for RMQ in rmq0 rmq1
+do
+    cp -vf "$certs_result_dir"/*"$RMQ"*.pem "$rmq_certs/$RMQ/"
+done
